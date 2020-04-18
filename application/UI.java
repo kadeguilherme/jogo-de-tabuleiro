f8783373e3e891ef6966bff72147e3665ceb6076
kadeguilherme <kadeguilherme@gmail.com>
2020-04-18 00:37:49 -0300

    Arrumando os espaço para colocar as peças no tabuleiro

--- application/UI.java
+++ application/UI.java
@@ -6,7 +6,7 @@ public class UI {
 	
 	public static void printTabuleiro(PecaDeXadrez[][] pecas ) {
 		for (int i = 0; i < pecas.length; i++) {
-			System.out.print((8 - i));
+			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pecas.length; j++) {
 				printPeca(pecas[i][j]);
 			}
@@ -18,10 +18,11 @@ public class UI {
 
 	public static void printPeca(PecaDeXadrez peca) {
 		if(peca == null) {
-			System.out.print(" -");
+			System.out.print("-");
 		}else {
 			System.out.print(peca);
 		}
+		System.out.print(" ");
 		
 	}
 }
